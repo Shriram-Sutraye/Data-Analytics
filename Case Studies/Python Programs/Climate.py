c1be88fc47a316057b0311927ecd6cb9732dd78b
Shriram Suthraye <shriramsuthraye@gmail.com>
2023-10-15 15:38:05 -0400

    Third Commit

---
+++ Case Studies/Python Programs/Climate.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mlt
+import seaborn as sns
+data = pd.read_csv("/home/shriram/Documents/Data Analytics/Case Studies/vjaFRMXv.csv")
+data.head(15)
