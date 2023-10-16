65fabc9b4849731b4fd7e11bf7683c70cb40e44a
Shriram Suthraye <shriramsuthraye@gmail.com>
2023-10-15 20:18:04 -0400

    after_some_analyis

--- Case Studies/Python Programs/Climate.py
+++ Case Studies/Python Programs/Climate.py
@@ -28,3 +28,39 @@ plt.ylabel('Tmin < -15°C')
 plt.title('Temperature Data under Different Scenarios')
 plt.legend()
 plt.show()
+
+for scenario in ['SSP1-2.6', 'SSP2-4.5', 'SSP5-8.5']:
+    for percentile in ['Median', 'Range (low)', 'Range (high)']:
+        column_name = f'{scenario} {percentile}'
+        plt.plot(data['DateTime'], data[column_name], label=f'{scenario} {percentile}')
+
+plt.xlabel('Time')
+plt.ylabel('Temperature Data')
+plt.title('Temperature Data under Different Scenarios and Percentiles')
+plt.legend()
+plt.show()
+
+
+plt.savefig('temperature_time_series.png')
+
+
+historical_data = data['Modeled Historical']
+days_below_minus_15_1= (historical_data < -15).sum()
+
+print(days_below_minus_15)
+
+# Filter data for the specific scenario (e.g., SSP1-2.6)
+scenario_data = data['SSP1-2.6 Median']  # Use the appropriate column name
+
+days_below_minus_15_2 = (scenario_data < -15).sum()
+
+print(days_below_minus_15_2)
+
+
+#Filtering based on the senario that it will not live more than 4 days in -15 degrees
+if days_below_minus_15 < 4:
+    print("The location is suitable for aquaculture.")
+else:
+    print("The location is not suitable for aquaculture.")
+
+
